d3e44b7b5b0d1b642e6c21691465f857aedfc772
Fletterio <fj.letterio@gmail.com>
2022-11-22 13:53:52 -0300

    slowly adding vector operations

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod vec3;
 
-use vec3::vec3::Vec3;
+use vec3::Vec3;
 
 fn main(){
-    let a = Vec3::new(1,2,3);
-    let b = -a;
-    println!("{}", b);
+    let a = Vec3::new(1f32,2f32,3f32);
+    let mut b = -a;
+    b[0] = 7f32;
+    println!("{} {} {}", b[0], b[1], b[2]);
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,36 +1,59 @@
-use std::ops::{Neg, Index, IndexMut, AddAssign, SubAssign, MulAssign, DivAssign};
+use std::ops::{AddAssign, DivAssign, Index, IndexMut, MulAssign, Neg, SubAssign};
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub struct Vec3 {
+    e: [f32; 3],
+}
 
-pub mod vec3 {
-    #[derive(Copy, Clone, PartialEq, Debug)]
-    pub struct Vec3 {
-        e : [f32; 3],
+//constructors
+impl Vec3 {
+    pub fn new(e0: f32, e1: f32, e2: f32) -> Self {
+        Vec3 { e: [e0, e1, e2] }
     }
-    
-    //constructors
-    impl Vec3 {
-        pub fn new(e0 : f32, e1 : f32, e2 : f32) -> Self {
-            Vec3 {e : [e0, e1, e2]}
-        }
+}
+
+//getters
+impl Vec3 {
+    pub fn x(self) -> f32 {
+        self.e[0]
     }
-
-    //getters
-    impl Vec3{
-        pub fn x(self){self.e[0]}
-        pub fn y(self){self.e[1]}
-        pub fn z(self){self.e[2]}
-        pub fn r(self){self.e[0]}
-        pub fn g(self){self.e[1]}
-        pub fn b(self){self.e[2]}
+    pub fn y(self) -> f32 {
+        self.e[1]
     }
+    pub fn z(self) -> f32 {
+        self.e[2]
+    }
+    pub fn r(self) -> f32 {
+        self.e[0]
+    }
+    pub fn g(self) -> f32 {
+        self.e[1]
+    }
+    pub fn b(self) -> f32 {
+        self.e[2]
+    }
+}
 
-    //operators
-    impl Neg for Vec3{
-        type Output = Self;
+//operators
+impl Neg for Vec3 {
+    type Output = Self;
 
-        fn neg(self) -> Self::Output{
-            Vec3{e : [-self.e[0], -self.e[1], -self.e[2]]}
+    fn neg(self) -> Self::Output {
+        Vec3 {
+            e: [-self.e[0], -self.e[1], -self.e[2]],
         }
     }
 }
 
+impl Index<usize> for Vec3{
+    type Output = f32;
 
+    fn index(&self, index : usize) -> &Self::Output{
+        &self.e[index]
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, index : usize) -> &mut Self::Output {
+        &mut self.e[index]
+    }
+}
