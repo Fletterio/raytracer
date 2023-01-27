a0f902766f6208c1db899c1b4a64abdbac66bc0b
Fletterio <fj.letterio@gmail.com>
2023-01-27 20:50:51 -0300

    How does folding on Rust work

--- src/renderer.rs
+++ src/renderer.rs
@@ -41,12 +41,12 @@ pub fn render(width : i32, height : i32, samples : i32, depth : i32, world : &im
         
         for i in 0..width{
 
-            let pixel_color = (0..samples).par_iter().map(|| {
+            let pixel_color : Color = (0..samples).into_par_iter().map(|_| {
                 let u = (i as f32 + random_double()) / (width as f32 - 1.0);
                 let v = (j as f32 + random_double()) / (height as f32 - 1.0);
                 let r = cam.get_ray(u,v);
                 ray_color(&r, world, depth)
-            }).fold(Color::new(0.0, 0.0, 0.0), |acc, elem| acc + elem);
+            }).fold(|| Color::new(0.0, 0.0, 0.0), |acc, elem| acc + elem);
 
             pixel_color.write_color(0.5, samples, &mut file);
             /*let mut pixel_color = Color::new(0.0, 0.0, 0.0);
