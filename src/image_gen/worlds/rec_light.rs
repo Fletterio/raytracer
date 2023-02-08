110011ec122ed7e4619ea7f6aec57fee08fb2881
Fletterio <fj.letterio@gmail.com>
2023-02-08 17:30:23 -0300

    playing around with lights

--- src/image_gen/worlds/rec_light.rs
+++ src/image_gen/worlds/rec_light.rs
@@ -25,13 +25,13 @@ pub fn light() -> HitableList {
         Arc::new(Lambertian::new(Arc::clone(&pertext))),
     )));
 
-    let difflight = Arc::new(DiffuseLight::from_color(Color::new(4.0, 4.0, 4.0)));
-    objects.add(Arc::new(XYRect::new(3.0, 5.0, 1.0, 3.0, -2.0, difflight)));
+    //let difflight = Arc::new(DiffuseLight::from_color(Color::new(4.0, 4.0, 4.0)));
+    //objects.add(Arc::new(XYRect::new(3.0, 5.0, 1.0, 3.0, -2.0, difflight)));
 
-    let redlight = Arc::new(DiffuseLight::from_color(Color::new(5.0, 0.0, 0.0)));
+    let redlight = Arc::new(DiffuseLight::from_color(Color::new(8.0, 0.0, 0.0)));
     objects.add(Arc::new(Sphere::new(
-        Point3::new(0.0, 6.0, 0.0),
-        2.5,
+        Point3::new(0.0, 20.0, 0.0),
+        10.0,
         redlight,
     )));
 
