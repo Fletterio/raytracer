acca28457c34f0cc26314311dd4ce0f9de3795f1
Fletterio <fj.letterio@gmail.com>
2023-02-08 17:20:52 -0300

    Adding more stuff to the light world

--- src/image_gen/worlds/rec_light.rs
+++ src/image_gen/worlds/rec_light.rs
@@ -28,5 +28,12 @@ pub fn light() -> HitableList {
     let difflight = Arc::new(DiffuseLight::from_color(Color::new(4.0, 4.0, 4.0)));
     objects.add(Arc::new(XYRect::new(3.0, 5.0, 1.0, 3.0, -2.0, difflight)));
 
+    let redlight = Arc::new(DiffuseLight::from_color(Color::new(5.0, 0.0, 0.0)));
+    objects.add(Arc::new(Sphere::new(
+        Point3::new(0.0, 6.0, 0.0),
+        2.5,
+        Arc::new(Lambertian::new(redlight)),
+    )));
+
     objects
 }
